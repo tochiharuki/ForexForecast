c2fd56859ed3aa52457a269f8af4ca4d0de6dc08
Haruki Tochishita <tochi.haruki@gmail.com>
2025-12-31 15:15:00 +0900

    Initial commit

---
+++ ForexForecast/ForexForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForexForecastApp.swift
+//  ForexForecast
+//
+//  Created by Tochishita Haruki on 2025/12/31.
+//
+
+import SwiftUI
+
+@main
+struct ForexForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForexForecastTests/ForexForecastTests.swift
@@ -0,0 +1,16 @@
+//
+//  ForexForecastTests.swift
+//  ForexForecastTests
+//
+//  Created by Tochishita Haruki on 2025/12/31.
+//
+
+import Testing
+
+struct ForexForecastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
